fbea5d607dfc8ae73e1ea03db3224e0f82dbe060
IannMatthews <iannmatthews@hotmail.com>
2021-08-31 10:29:21 -0300

    refactor: adjust encrypt return

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -10,6 +10,6 @@ export class JwtAdapter implements Encrypter {
 
   async encrypt (value: string): Promise<string> {
     const accessToken = await jwt.sign({ id: value }, this.secret)
-    return new Promise(resolve => resolve(accessToken))
+    return accessToken
   }
 }
